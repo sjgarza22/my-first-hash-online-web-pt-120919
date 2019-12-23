dafed6ec68d165c6e3f909a15faf490ccb26d50b
Sara Garza <sjgarza22@gmail.com>
2019-12-23 00:50:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,11 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_hash = {
+          "name" => "Sara Garza",
+          "job" => "IT Support Technician",
+          "likes" => "Books & Baby Yoda",
+          "dislikes" => "Licorice"
+  }
   
 end
 
